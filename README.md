fe3b9a181d864788bae890efe087f5ac51b97046
Kye <kye@apacmediasolutions.com>
2023-05-21 00:37:40 -0400

    v1 and v2

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# tree-of-thoughts
-Implementation of Tree of Thoughts: Deliberate Problem Solving with Large Language Models
+# Tree of Thoughts 🌳🌲🌴🌿🍃
+
+Tree of Thoughts (ToT) is a powerful and flexible algorithm for leveraging pre-trained language models to solve various problems by exploring multiple reasoning paths. It's designed to be plug-and-play, allowing users to easily connect their models and use the Tree of Thoughts method.
+
+🌟 Features:
+- General problem-solving framework for language models
+- Supports both breadth-first search (BFS) and depth-first search (DFS) algorithms
+- Easy integration with popular language models like OpenAI and Hugging Face
+- Extensible and adaptable to different problem properties and resource constraints
+
+## Algorithmic Pseudocode
+
+1. Define the thought decomposition based on the problem properties.
+2. Create a thought generator function G(pθ, s, k) with two strategies:
+   a. Sample i.i.d. thoughts from a CoT prompt.
+   b. Propose thoughts sequentially using a "propose prompt".
+3. Create a state evaluator function V(pθ, S) with two strategies:
+   a. Value each state independently.
+   b. Vote across states.
+4. Choose a search algorithm (BFS or DFS) based on the tree structure.
+5. Implement the chosen search algorithm.
+6. Execute the chosen search algorithm with the input problem, thought generator, state evaluator, and other required parameters.
+
+## Usage Examples
+
+### OpenAI API
+
+To use Tree of Thoughts with OpenAI's API, create a custom model class that inherits from `AbstractLanguageModel` and implements the required methods using OpenAI's API. Then, create an instance of the `TreeOfThoughts` class with the custom model and the desired search algorithm ('BFS' or 'DFS').
+
+### Hugging Face Transformers
+
+To use Tree of Thoughts with Hugging Face Transformers, create a custom model class that inherits from `AbstractLanguageModel` and implements the required methods using Hugging Face Transformers. Then, create an instance of the `TreeOfThoughts` class with the custom model and the desired search algorithm ('BFS' or 'DFS').
+
+## Roadmap
+
+### Multi-Modality Tree of Thoughts 🌐🌳
+
+The next big advancement for the Tree of Thoughts algorithm is to extend it to multi-modality, enabling it to handle not only text but also images, audio, and other data types. This will bring us closer to multi-modal superintelligence.
+
+#### Actionable Steps
+
+1. Research and identify suitable multi-modal pre-trained models that can handle various data types (e.g., text, images, audio).
+2. Adapt the thought decomposition, thought generator, and state evaluator functions to handle multi-modal data.
+3. Develop a method for combining different modalities in the search tree, allowing the algorithm to reason across different data types.
+4. Implement and test the multi-modal Tree of Thoughts algorithm with various problems and datasets.
+5. Optimize the algorithm for performance and resource usage, ensuring it scales well with large multi-modal datasets.
+6. Publish the results and gather feedback from the community to further improve the multi-modal Tree of Thoughts algorithm.
+
+Join us on this exciting journey to advance the Tree of Thoughts algorithm to multi-modality superintelligence! 🚀
---
+++ v2.py
@@ -0,0 +1,117 @@
+from abc import ABC, abstractmethod
+import openai
+
+class AbstractLanguageModel(ABC):
+    @abstractmethod
+    def generate_thoughts(self, state, k):
+        pass
+
+    @abstractmethod
+    def evaluate_states(self, states):
+        pass
+
+
+class CustomLanguageModel(AbstractLanguageModel):
+    def __init__(self, model):
+        self.model = model
+
+    def generate_thoughts(self, state, k):
+        #implement the thought generation logic using self.model
+        pass
+
+    def evaluate_states(self, states):
+        #implement state evaluation logic using self.model
+        pass
+
+class OpenAILanguageModel(AbstractLanguageModel):
+    def __init__(self, api_key):
+        openai.api_key = api_key
+
+    def generate_thoughts(self, state, k):
+        #implement logic
+        pass
+
+    def evaluate_states(self, states):
+        pass
+
+
+class TreeofThoughts:
+    """
+    1. Thought Decomposition --> based on problem properties
+
+    2. Thought Generator -> create a thought generator function G(p0, s, k) with 2 strategies a sample iid thoughts from a cot prompt b. propose thoughts
+    sequentially using a propose prompt
+
+    3. create a state evaluator function V(p0, S) with 2 strategies a value each state independently b. vote across states
+
+    4. Choose a search algo based on tree structure [BFS or DFS]
+
+    Implement chosen search algorithm for bfs (algo1):
+        init S0 with the input x
+        for t = 1 to T (step limit):
+            generate candidate thoughts for each state in St-1
+            eveluate the candiate states using the state evaluator V
+            select the b most promising states for St
+
+        return the final output by genertaing the thought for the best state in St for DFS(algo2)
+
+        defien a recurseive DFS function with the current state s, step t, and other required params
+
+        if t > T record the output by generating the thought for current state S
+
+        for each candidate state s in the sorted list of generated thoughts for s:
+            
+            if the evaluated value of s is greater the the threshold of vth call the dfs function recursively
+            with s and t + 1
+
+    execute the chosen search algo with the input problem, thought generator, and state evaluator, and other required params
+
+
+    """
+    
+    def __init__(self, model, search_algorithm):
+        self.model = model
+        self.search_algorithm = search_algorithm
+
+    def solve(self, x, k, T, b, vth):
+        if self.search_algorithm == 'BFS':
+            return self.tot_bfs(x, k, T, b)
+        elif self.search_algorithm == 'DFS':
+            return self.tot_dfs(x, k, T, vth)
+        else:
+            raise ValueError("Invalid search algorithm. Choose 'BFS' or 'DFS'.")
+
+    def tot_bfs(self, x, k, T, b):
+        S0 = {x}
+        for t in range(1, T + 1):
+            S0_t = {(*s, z) for s in S0 for z in self.model.generate_thoughts(s, k)}
+            Vt = self.model.evaluate_states(S0_t)
+            St = sorted(S0_t, key=lambda s: Vt[s], reverse=True)[:b]
+            S0 = set(St)
+        return self.model.generate_thoughts(max(St, key=lambda s: Vt[s]), 1)
+
+    def tot_dfs(self, x, k, T, vth):
+        output = []
+
+        def dfs(s, t):
+            if t > T:
+                output.append(self.model.generate_thoughts(s, 1))
+                return
+            for s_prime in sorted(self.model.generate_thoughts(s, k)):
+                if self.model.evaluate_states({s_prime})[s_prime] > vth:
+                    dfs((*s, s_prime), t + 1)
+
+        dfs(x, 1)
+        return output
+    
+
+
+
+# usage
+openai_model = OpenAILanguageModel('sk')
+
+#choose search algorithm('bfs or 'dfs)
+search_algorithm = "BFS"
+
+#create an instance of the tree of thoughts class
+tree_of_thoughts= TreeofThoughts(openai_model, search_algorithm)
