0ca0389249a9be2aafcb3b6aea118bf82d1dfc06
Kye <kye@apacmediasolutions.com>
2023-05-22 11:44:19 -0400

    version nymber setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tree-of-thoughts',
   packages = find_packages(exclude=[]),
-  version = '0.1.4',
+  version = '0.1.5',
   license='MIT',
   description = 'Tree of Thoughts - Pytorch',
   author = 'Kye Gomez',
