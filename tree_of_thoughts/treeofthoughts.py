c5f136f25e10170437ef58cf041175871dd02c2e
yhyu13 <yohan680919@gmail.com>
2023-05-23 01:45:18 +0800

    Add chat complretion support (Still, need to control rate limit, will always hit rate limit on gpt 4 currently)

--- tree_of_thoughts/treeofthoughts.py
+++ tree_of_thoughts/treeofthoughts.py
@@ -2,6 +2,7 @@ import concurrent.futures
 from abc import ABC, abstractmethod
 import openai
 import os
+import re
 import time
 
 class AbstractLanguageModel(ABC):
@@ -39,7 +40,7 @@ class OpenAILanguageModel(AbstractLanguageModel):
         if api_base != "":
             # e.g. https://api.openai.com/v1/ or your custom url
             openai.api_base = api_base
-            print(f'Using custom api_base {self.api_base}')
+            print(f'Using custom api_base {api_base}')
             
         if api_model == "" or api_model == None:
             api_model = os.environ.get("OPENAI_API_MODEL", "")
@@ -49,28 +50,79 @@ class OpenAILanguageModel(AbstractLanguageModel):
             self.api_model = "text-davinci-003"
         print(f'Using api_model {self.api_model}')
 
+        self.use_chat_api = 'gpt' in self.api_model
+
         # reference : https://www.promptingguide.ai/techniques/react
         self.ReAct_prompt = ''
         if enable_ReAct_prompting:
-            self.ReAct_prompt = "Write down your observations in format 'Observation : \\n xxxx \\n', then write down your thoughts in format 'Thoughts : \\n xxxx \\n'."
+            self.ReAct_prompt = "Write down your observations in format 'Observation:xxxx', then write down your thoughts in format 'Thoughts:xxxx'."
         
         self.strategy = strategy
         self.evaluation_strategy = evaluation_strategy
+
+    def openai_api_call_handler(self, prompt, max_tokens, temperature, k=1, stop=None):
+        if self.use_chat_api:
+            message = [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+            response = openai.ChatCompletion.create(
+                model=self.api_model,
+                messages=message,
+                max_tokens=max_tokens,
+                temperature=temperature,
+            )
+        else:
+            response = openai.Completion.create(
+                engine=self.api_model,
+                prompt=prompt,
+                n=k,
+                max_tokens=max_tokens,
+                stop=stop,
+                temperature=temperature,
+            )
+        return response
+
+    def openai_choice2text_handler(self, choice):
+        if self.use_chat_api:
+            text = choice['message']['content']
+        else:
+            text = choice.text.strip()
+        return text
 
     def generate_thoughts(self, state, k):
         state_text = ' '.join(state)
         
         prompt = f"Given the current state of reasoning: '{state_text}', generate {k} coherent thoughts to continue the reasoning process:"
         prompt += self.ReAct_prompt
-        response = openai.Completion.create(
-            engine=self.api_model,
-            prompt=prompt,
-            n=k,
-            max_tokens=50,
-            stop=None,
-            temperature=0.5,
-        )
-        thoughts = [choice.text.strip() for choice in response.choices]
+        if self.use_chat_api:
+            new_prompt_success = False
+            """
+            # Try prompt and parse in a single shot to save tokens (but if we fail, we end up spending more tokens)
+            new_prompt = prompt + "Thought string should be output in a format that can be parsed into python array in format [xxx,xxx,xxx]"
+            response = self.openai_api_call_handler(new_prompt, 100 * k, 0.5, 1)
+            text = self.openai_choice2text_handler(response.choices[0])
+            re_parse = re.search(r'\[(.*?)\]', text)
+            if re_parse:
+                thoughts_str = re_parse.group(1)
+                if thoughts_str:
+                    thoughts = thoughts_str.split(',')
+                    new_prompt_success = len(thoughts) == k 
+                    if not new_prompt_success:
+                        print(f"Fall back to multi-prompt for chat-completion due to parse fail {text}")
+            """
+
+            if not new_prompt_success:
+                thoughts = []
+                for _ in range(k):
+                    response = self.openai_api_call_handler(prompt, 100, 0.5, 1)
+                    text = self.openai_choice2text_handler(response.choices[0])
+                    thoughts += [text]
+        else:
+            response = self.openai_api_call_handler(prompt, 50, 0.5, k)
+            thoughts = [self.openai_choice2text_handler(choice) for choice in response.choices]
         # print(thoughts)
         print(f"Generated thoughts: {thoughts}")
         return thoughts
@@ -81,18 +133,11 @@ class OpenAILanguageModel(AbstractLanguageModel):
             for state in states:
                 state_text = ' '.join(state)
                 prompt = f"Given the current state of reasoning: '{state_text}', evaluate its value as a float between 0 and 1, and NOTHING ELSE:"
-                response = openai.Completion.create(
-                    engine=self.api_model,
-                    prompt=prompt,
-                    n=1,
-                    max_tokens=10,
-                    stop=None,
-                    temperature=1,
-                )
+                response = self.openai_api_call_handler(prompt, 10, 1)
                 try:
-                    value_text = response.choices[0].text.strip()
+                    value_text = self.openai_choice2text_handler(response.choices[0])
                     print(f"Value text {value_text}")
-                    value = float(response.choices[0].text.strip())
+                    value = float(value_text)
                     print(f"value: {value}")
                 except ValueError:
                     value = 0  # Assign a default value if the conversion fails
@@ -102,15 +147,8 @@ class OpenAILanguageModel(AbstractLanguageModel):
         elif self.evaluation_strategy == 'vote':
             states_text = '\n'.join([' '.join(state) for state in states])
             prompt = f"Given the following states of reasoning, vote for the best state:\n{states_text}\n\nVote, and NOTHING ELSE:"
-            response = openai.Completion.create(
-                engine=self.api_model,
-                prompt=prompt,
-                n=1,
-                max_tokens=50,
-                stop=None,
-                temperature=1,
-            )
-            best_state_text = response.choices[0].text.strip()
+            response = self.openai_api_call_handler(prompt, 50, 1)
+            best_state_text = self.openai_choice2text_handler(response.choices[0])
             print(f"Best state text: {best_state_text}")
             best_state = tuple(best_state_text.split())
             return {state: 1 if state == best_state else 0 for state in states}
