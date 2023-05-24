16caed97ccbde9cab9f2aaa1598db7efbd9e39c9
mengban <mengban2@gmail.com>
2023-05-24 11:03:30 +0800

    fix incorrect indentation in example.py
    Incorrect indentation of main processing flow

--- example.py
+++ example.py
@@ -27,21 +27,21 @@ else:
     #or v2 -> dynamic beam width -< adjust the beam width [b] dynamically based on the search depth quality of the generated thoughts
     tree_of_thoughts= OptimizedTreeofThoughts(model, search_algorithm)
 
-    input_problem = "use 4 numbers and basic arithmetic operations (+-*/) to obtain 24"
-    k = 5
-    T = 3
-    b = 5
-    vth = 0.5
-    timeout = 10
-    confidence = 1.0 #cmodel is confident on performance
-    max_iterations = 40 #tree branh nodes 
-    convergence_threshold = 0.01
-    convergence_count = 5
+input_problem = "use 4 numbers and basic arithmetic operations (+-*/) to obtain 24"
+k = 5
+T = 3
+b = 5
+vth = 0.5
+timeout = 10
+confidence = 1.0 #cmodel is confident on performance
+max_iterations = 40 #tree branh nodes 
+convergence_threshold = 0.01
+convergence_count = 5
 
 
 
-    
-    solution = tree_of_thoughts.solve(input_problem, k, T, b, vth, timeout, confidence_threshold=confidence, max_iterations=max_iterations, convergence_threshold=convergence_threshold, convergence_count=convergence_count)
+
+solution = tree_of_thoughts.solve(input_problem, k, T, b, vth, timeout, confidence_threshold=confidence, max_iterations=max_iterations, convergence_threshold=convergence_threshold, convergence_count=convergence_count)
     
 #use the solution in your production environment
 print(f"solution: {solution}")
