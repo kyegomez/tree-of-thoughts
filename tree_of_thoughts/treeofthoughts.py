228024e6fb62b6a961059ab91e112b76bfafa04a
yhyu13 <yohan680919@gmail.com>
2023-05-24 19:53:11 +0800

    Add guidance prompt format (WIP) still need fine tune prompt, GPT4 actually not following the exact output for value evaluation

--- tree_of_thoughts/treeofthoughts.py
+++ tree_of_thoughts/treeofthoughts.py
@@ -28,7 +28,7 @@ class CustomLanguageModel(AbstractLanguageModel):
         #implement state evaluation logic using self.model
         pass
 class OpenAILanguageModel(AbstractLanguageModel):
-    def __init__(self, api_key, strategy="cot", evaluation_strategy="value", api_base="", api_model="", enable_ReAct_prompting=True):
+    def __init__(self, api_key, strategy="cot", evaluation_strategy="value", api_base="", api_model="", enable_ReAct_prompting=False):
         if api_key == "" or api_key == None:
             api_key = os.environ.get("OPENAI_API_KEY", "")
         if api_key != "":
@@ -165,7 +165,7 @@ class OpenAILanguageModel(AbstractLanguageModel):
             raise ValueError("Invalid evaluation strategy. Choose 'value' or 'vote'.")
 
 class OptimizedOpenAILanguageModel(OpenAILanguageModel):
-    def __init__(self, api_key, strategy="cot", evaluation_strategy="value", cache_enabled=True, api_base="", api_model="", enable_ReAct_prompting=True):
+    def __init__(self, api_key, strategy="cot", evaluation_strategy="value", cache_enabled=True, api_base="", api_model="", enable_ReAct_prompting=False):
         super().__init__(api_key, strategy, evaluation_strategy, api_base, api_model, enable_ReAct_prompting)
         self.cache_enabled = cache_enabled
         self.thought_cache = {}
@@ -184,20 +184,122 @@ class OptimizedOpenAILanguageModel(OpenAILanguageModel):
         return state_values
     
 class GuidanceLanguageModel(AbstractLanguageModel):
-    def __init__(self, model, strategy="cot", evaluation_strategy="value"):
-        # guidance.llms.OpenAI("gpt-4")
+    def __init__(self, model, strategy="cot", evaluation_strategy="value", enable_ReAct_prompting=False):
+        # gpt4 = guidance.llms.OpenAI("gpt-4")
+        # vicuna = guidance.llms.transformers.Vicuna("your_path/vicuna_13B", device_map="auto")
         self.model = model
+        
+        # reference : https://www.promptingguide.ai/techniques/react
+        self.ReAct_prompt = ''
+        if enable_ReAct_prompting:
+            self.ReAct_prompt = '''{{#assistant~}}
+            {{gen 'Observation' temperature=0.5 max_tokens=50}}
+            {{~/assistant}}'''
+        
+        self.strategy = strategy
+        self.evaluation_strategy = evaluation_strategy
+        
+        self.thoughts_program = guidance('''
+            {{#system~}}
+            You are a logical and rational assistant.
+            {{~/system}}
+
+            {{#user~}}
+            Given the current state of reasoning:
+            {{state_text}}
+            Generate {{k}} coherent thoughts as short as possible to continue the reasoning process.
+            Don't answer the question yet.
+            {{~/user}}
+
+            %s
+            
+            {{#assistant~}}
+            {{gen 'Thoughts' temperature=0.5 max_tokens=50}}
+            {{~/assistant}}
+            ''' % self.ReAct_prompt, llm=self.model)
+        
+        self.value_program = guidance('''
+            {{#system~}}
+            You are a logical and rational assistant.
+            {{~/system}}
+
+            {{#user~}}
+            Given the current state of reasoning:
+            {{state_text}}
+            Evaluate its value as a float between 0 and 1, and NOTHING ELSE
+            Don't answer the question yet.
+            {{~/user}}
+
+            {{#assistant~}}
+            {{gen 'Value' temperature=1 max_tokens=10}}
+            {{~/assistant}}
+            ''', llm=self.model)
+        
+        self.vote_program = guidance('''
+            {{#system~}}
+            You are a logical and rational assistant.
+            {{~/system}}
+
+            {{#user~}}
+            Given the following states of reasoning, vote for the best state:
+            {{states_text}}
+            Give the index of your voted best state(the 1st state has index 0), and NOTHING ELSE
+            Don't answer the question yet.
+            {{~/user}}
+
+            {{#assistant~}}
+            {{gen 'Vote' temperature=1 max_tokens=10}}
+            {{~/assistant}}
+            ''', llm=self.model)
+        
+    def model_response_handler(self, program, **kargs):
+        print("Calling guidance model(Modify Me to handle specific LLM response excpetions!)")
+        reponse = program(**kargs)
+        return reponse
 
     def generate_thoughts(self, state, k):
         #implement the thought generation logic using self.model
-        pass
+        state_text = ' '.join(state)
+        
+        thoughts = []
+        for _ in range(k):
+            response = self.model_response_handler(self.thoughts_program, state_text=state_text, k=1)
+            text = response['Thoughts']
+            thoughts += [text]
+        # print(thoughts)
+        print(f"Generated thoughts: {thoughts}")
+        return thoughts
 
     def evaluate_states(self, states):
         #implement state evaluation logic using self.model
-        pass
+        if self.evaluation_strategy == 'value':
+            state_values = {}
+            for state in states:
+                state_text = ' '.join(state)
+                response = self.model_response_handler(self.value_program, state_text=state_text)
+                try:
+                    value_text = response['Value']
+                    print(f"Value text {value_text}")
+                    value = float(value_text)
+                    print(f"value: {value}")
+                except ValueError:
+                    value = 0  # Assign a default value if the conversion fails
+                state_values[state] = value
+            return state_values
+
+        elif self.evaluation_strategy == 'vote':
+            states_text = '\n'.join([' '.join(state) for state in states])
+            response = self.model_response_handler(self.vote_program, states_text=states_text)
+            best_state_text = response['Vote']
+            print(f"Best state text: {best_state_text}")
+            best_state = int(best_state_text)
+            return {state: 1 if i == best_state else 0 for i in range(len(states))}
+
+        else:
+            raise ValueError("Invalid evaluation strategy. Choose 'value' or 'vote'.")
 
 class GuidanceOpenAILanguageModel(GuidanceLanguageModel):
-    def __init__(self, api_key, strategy="cot", evaluation_strategy="value", api_base="", api_model=""):
+    def __init__(self, api_key, strategy="cot", evaluation_strategy="value", api_base="", api_model="", enable_ReAct_prompting=False):
         if api_key == "" or api_key == None:
             api_key = os.environ.get("OPENAI_API_KEY", "")
         if api_key != "":
@@ -220,7 +322,30 @@ class GuidanceOpenAILanguageModel(GuidanceLanguageModel):
             self.api_model = "text-davinci-003"
         print(f'Using api_model {self.api_model}')
         
-        super.__init__(guidance.llms.OpenAI(self.api_model), strategy, evaluation_strategy)
+        super().__init__(guidance.llms.OpenAI(self.api_model), strategy, evaluation_strategy, enable_ReAct_prompting)
+        
+    
+    def model_response_handler(self, program, **kargs):
+        error_msg = ''
+        while True:
+            try:
+                program.llm.max_retries = 60
+                guidance.llms.OpenAI.cache.clear()
+                response = program(**kargs)
+                return response
+            except openai.error.RateLimitError as e:
+                sleep_duratoin = os.environ.get("OPENAI_RATE_TIMEOUT", 30)
+                print(f'{str(e)}, sleep for {sleep_duratoin}s, set it by env OPENAI_RATE_TIMEOUT')
+                time.sleep(sleep_duratoin)
+            except Exception as e:
+                if str(e) == f'''Too many (more than {guidance.llm.max_retries}) OpenAI API RateLimitError's in a row!''':
+                    sleep_duratoin = os.environ.get("OPENAI_RATE_TIMEOUT", 30)
+                    print(f'{str(e)}, sleep for {sleep_duratoin}s, set it by env OPENAI_RATE_TIMEOUT')
+                    time.sleep(sleep_duratoin)
+                else:
+                    error_msg = str(e)
+                    break
+        raise Exception(error_msg)
 
 class TreeofThoughts:
     """
