a6429d8ed2f492d44555b98616f637c0e63ebd47
Kye <kye@apacmediasolutions.com>
2023-05-25 22:54:00 -0400

    1st

--- tree_of_thoughts/treeofthoughts.py
+++ tree_of_thoughts/treeofthoughts.py
@@ -7,6 +7,8 @@ import re
 import guidance
 import time
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
+
 
 DATA_PATH = './data'
 
@@ -88,6 +90,60 @@ class HuggingLanguageModel(AbstractLanguageModel):
             state_values[state] = value
 
         return state_values
+
+
+class HFPipelineModel(AbstractLanguageModel):
+    def __init__(self, model_name, verbose=False):
+        self.pipeline = pipeline("text-generation", model=model_name)
+        self.verbose = verbose
+
+    def generate_thoughts(self, state, k, max_length=100):
+        state_text = ' '.join(state)
+        prompt = f"Write down your observations in format 'Observation:xxxx', then write down your thoughts in format 'Thoughts:xxxx Given the current state of reasoning: '{state_text}', generate {k} coherent solutions to achieve {state_text}"
+
+        if self.verbose:
+            print(f"Generating thoughts for state: {state_text}")
+
+        try:
+            generated_outputs = self.pipeline(prompt, max_length=max_length, num_return_sequences=k)
+            thoughts = [output["generated_text"] for output in generated_outputs]
+            print(f'thoughts: {thoughts}')
+        except Exception as e:
+            if self.verbose:
+                print(f"Error generating thoughts for state: {state_text}")
+                print(f"Error: {e}")
+            thoughts = []
+
+        return thoughts
+
+    def evaluate_states(self, states, initial_prompt, max_length=10):
+        state_values = {}
+        for state in states:
+            state_text = ' '.join(state)
+            prompt = f"Given the current state of reasoning: '{state_text}', pessimistically evaluate its value as a float between 0 and 1 based on its potential to achieve {initial_prompt}"
+
+            if self.verbose:
+                print(f"Evaluating state: {state_text}")
+
+            try:
+                generated_outputs = self.pipeline(prompt, max_length=max_length, num_return_sequences=1)
+                value_text = generated_outputs[0]["generated_text"]
+                value = float(value_text)
+                print(f'value {value}')
+            except ValueError:
+                if self.verbose:
+                    print(f"Error converting value to float for state: {state_text}")
+                value = 0  # Assign a default value if the conversion fails
+            except Exception as e:
+                if self.verbose:
+                    print(f"Error evaluating state: {state_text}")
+                    print(f"Error: {e}")
+                value = 0
+
+            state_values[state] = value
+
+        return state_values
+        
 
 
 
