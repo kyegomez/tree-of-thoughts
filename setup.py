dd2cd97fd4ce13833021982fd1de92a0475d1d7d
Kye <kye@apacmediasolutions.com>
2023-05-30 09:45:11 -0400

    setup py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tree-of-thoughts',
   packages = find_packages(exclude=[]),
-  version = '0.2.8',
+  version = '0.2.9',
   license='MIT',
   description = 'Tree of Thoughts - Pytorch',
   author = 'Kye Gomez',
